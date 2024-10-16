2dd84d92d6755649ac6709dffb25e22865f718a5
Adarsh Agrawal <155928266+adarshzip@users.noreply.github.com>
2024-10-15 18:56:30 -0700

    many many cahnges
    added skelo elo skelo glicko bid curve to scuffed glicko and some more utilities

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import glob
 import pandas as pd
 import numpy as np
 import math
+from skelo.model.elo import EloEstimator
+from skelo.model.glicko2 import Glicko2Estimator
 
 '''
 Notes:
@@ -9,7 +11,31 @@ Must include file with entries in the tournament folder, then two folders
 with all of the prelims and elims in order
 '''
 
+# globals -- dd
+bidList = {}
+elos_dict = {}
+
+# globals -- sidebias
+rounds = 0
+elims = 0
+negWs = 0
+elimNegWs = 0
+
+# globals -- glicko
+rounds_dict = {}
+roundCSV = "tournament,winner_name,winner_school,winner_id,loser_name,loser_school,loser_id,date\n"
+
+# globals -- misc
+unifiedEntriesDict = {}
+unifiedByName = {}
+playerid = 100000
+
 def entry_dict(tournament):
+    # glicko2 dict
+    global unifiedEntriesDict
+    global unifiedByName
+    global playerid
+    
     '''make a dictionary with all of the entries at a tournament with school and names'''
     outputDict = {}
     
@@ -21,6 +47,7 @@ def entry_dict(tournament):
     teams = teams.to_numpy()
     
     for team in teams:
+        playerid += 1
         school, name = team[1], team[0]
         if name == "Michael Meng":
             if "Lawrenceville" in school:
@@ -28,22 +55,115 @@ def entry_dict(tournament):
             else:
                 name = "Michael Meng (Strake)"
         outputDict[school] = [name, school]
+        
+        if school not in unifiedEntriesDict:
+            unifiedEntriesDict[school] = [name, school, playerid]
+            unifiedByName[name] = school
     
     print("Processed entries for " + tournament)    
     
     return outputDict
 
-bidList = {}
-K = 30
 
-rounds = 0
-elims = 0
-negWs = 0
-elimNegWs = 0
+def roundDict(tournament, date):
+    global unifiedEntriesDict
+    global roundCSV
 
-elos_dict = {}
+    files = glob.glob("Documents/Debate/SideBias/" + tournament + "/Prelims/*.csv")
+    
+    if len(files) == 0:
+        raise Exception("Error in reading prelims from {}.".format(tournament))
+    
+    for file in files:
+        file = open(file, "r", encoding="Latin-1")
+        
+        for line in file.readlines()[1:]:
+            line = line.split(",")
+            team1, team2, judge, result = tuple(line[0:4])
+            
+            result = result.lower()
+            
+            if "bye" in result or "bye" in team1 or "bye" in team2 or "bye" in judge or judge == "" or "fft" in result:
+                continue
+ 
+            if "neg" in result or "con" in result:
+                team1, team2 = team2, team1  # team 1 is the winning team
 
-def glickoPrelims(tournament, teamsDict, elos_dict, bid):
+            try:
+                team1, team2 = unifiedEntriesDict[team1], unifiedEntriesDict[team2]
+            except:
+                continue
+            
+            name1 = team1[0]
+            school1 = team1[1]
+            id1 = team1[2]
+            name2 = team2[0]
+            school2 = team2[1]
+            id2 = team2[2]    
+            roundCSV += tournament + "," + name1 + "," + school1 + "," + str(id1) + "," + name2 + "," + school2 + "," + str(id2) + "," + str(date) + "\n"
+            
+def roundDictCSV():
+    global roundCSV
+    
+    with open("Documents/Debate/SideBias/Rounds.csv", "w") as fp:
+        fp.write(roundCSV[:-1])
+
+def skeloElo():
+    global unifiedByName
+    
+    df = pd.read_csv("Documents/Debate/SideBias/Rounds.csv")
+    labels = len(df) * [1]
+    
+    model = EloEstimator(
+        key1_field="winner_name",
+        key2_field="loser_name",
+        timestamp_field="date",
+        initial_time=20240801
+    ).fit(df, labels)
+    
+    eloRankings = model.rating_model.to_frame()
+    
+    filterDate = eloRankings['valid_to'].isnull()
+    eloRankings = eloRankings[filterDate]
+    eloRankings = eloRankings.drop(labels='valid_from', axis=1)
+    eloRankings = eloRankings.drop(labels='valid_to', axis=1)
+    eloRankings = eloRankings.sort_values(by='rating', ascending=False).reset_index(drop=True)
+    eloRankings.index += 1
+    
+    eloRankings['schoolcol'] = eloRankings['key'].map(unifiedByName)
+    
+    eloRankings.to_csv('Documents/Debate/SideBias/elorerank.csv')
+    
+def skeloGlicko():
+    global unifiedByName
+    
+    df = pd.read_csv("Documents/Debate/SideBias/Rounds.csv")
+    labels = len(df) * [1]
+    
+    model = Glicko2Estimator(
+        key1_field="winner_name",
+        key2_field="loser_name",
+        timestamp_field="date",
+        initial_time=20240801
+    ).fit(df, labels)
+    
+    eloRankings = model.rating_model.to_frame()
+    
+    filterDate = eloRankings['valid_to'].isnull()
+    eloRankings = eloRankings[filterDate]
+    eloRankings = eloRankings.drop(labels='valid_from', axis=1)
+    eloRankings = eloRankings.drop(labels='valid_to', axis=1)
+    eloRankings.index += 1
+    eloRankings['school'] = eloRankings['key'].map(unifiedByName)
+    eloRankings[['rating2', 'rating deviation', 'rating volatility']] = eloRankings['rating'].apply(pd.Series)
+    eloRankings.drop(labels='rating', axis=1, inplace=True)
+    eloRankings.rename(columns={'rating2' : 'rating'}, inplace=True)
+    eloRankings = eloRankings.sort_values(by='rating', ascending=False).reset_index(drop=True)
+    
+    eloRankings.to_csv('Documents/Debate/SideBias/glickorank.csv')
+    
+
+def scuffedGlickoPrelims(tournament, teamsDict, elos_dict, bid):
     # Adds tournament prelims based off the glicko ranking system WITHOUT volatility calculations
     
     global rounds, elims, negWs, elimNegWs
@@ -101,30 +221,31 @@ def glickoPrelims(tournament, teamsDict, elos_dict, bid):
             sig2 = rd_team2/173.7178
             
             # player 1 calcs
-            gsig1 = 1/(math.sqrt(1 + (3*(sig2**2)/(math.pi**2))))
-            ef1 = 1/(1 + pow(math.e, (-gsig1 * (mu1 - mu2))))
-            v1 = 1/((gsig1**2)*ef1*(1-ef1))
-            d1 = gsig1 * (1 - ef1) # should be 0 -ef1 for a loss
+            gsig1 = 1 / (math.sqrt(1 + (3*(sig2**2)/(math.pi**2))))
+            ef1 = 1 / (1 + pow(math.e, ((-gsig1) * (mu1 - mu2))))
+            v1 = 1 / (pow(gsig1, 2) * ef1 * (1 - ef1))
+            d1 = gsig1 * (1 - ef1) # should be 0 - ef1 for a loss
             # glicko would update player volatility here. I'm keeping it constant at .06.
             
             sig1 = math.sqrt(sig1**2 + math.pow(0.06,2))
-            sig1 = 1/math.sqrt((1/sig1) + (1/v1))
+            sig1 = 1 / math.sqrt((1/sig1) + (1/v1))
             mu1 = mu1 + (sig1**2 * d1)
             
             # player 2 calcs
-            gsig2 = 1/math.sqrt(1 + (3*(sig1**2)/(math.pi**2)))
-            ef2 = 1/(1 + pow(math.e, (-gsig2 * (mu2 - mu1))))
-            v2 = 1/((gsig2**2)*ef2*(1-ef2))
-            d2 = gsig2 * (0 - ef2) # should be 0 -ef1 for a loss
+            gsig2 = 1 / math.sqrt(1 + (3*(sig1**2)/(math.pi**2)))
+            ef2 = 1 / (1 + pow(math.e, (-gsig2 * (mu2 - mu1))))
+            v2 = 1 / ((gsig2**2) * ef2 * (1-ef2))
+            d2 = gsig2 * (0 - ef2) # should be 0 - ef2 for a loss
             # glicko would update player volatility here. I'm keeping it constant at .06.
             
             sig2 = math.sqrt(sig2**2 + math.pow(0.06,2))
-            sig2 = 1/math.sqrt((1/sig2) + (1/v2))
+            sig2 = 1 / math.sqrt((1/sig2) + (1/v2))
             mu2 = mu2 + (sig2**2 * d2)
             
             # updating ELOs
             eloDelta1 = 173.7178 * mu1 
             eloDelta2 = 173.7178 * mu2 
+            
             
             elo_team1 = eloDelta1 + 1500
             rd_team1 = 173.7178 * sig1
@@ -137,7 +258,7 @@ def glickoPrelims(tournament, teamsDict, elos_dict, bid):
         file.close()
     return elos_dict
     
-def glickoElims(tournament, teamsDict, elos_dict, bid):
+def scuffedGlickoElims(tournament, teamsDict, elos_dict, bid):
     '''adds the elims of a tournament to the rankings'''
     
     global rounds, elims, negWs, elimNegWs
@@ -300,11 +421,12 @@ def eloAdjustedWinElims(tournament, teamsDict, elos_dict, bid): # will work on l
     return elos_dict
 
 
-def add_tournament(tournament, bid):
+def add_tournament(tournament, bid, date):
     '''adds a tournament to the rankings'''
     dictionary = entry_dict(tournament)
-    glickoPrelims(tournament, dictionary, elos_dict, bid)
-    glickoElims(tournament, dictionary, elos_dict, bid)
+    roundDict(tournament, date)
+    scuffedGlickoPrelims(tournament, dictionary, elos_dict, bid)
+    scuffedGlickoElims(tournament, dictionary, elos_dict, bid)
 
 
 def write_to_csv(elosList):
@@ -321,14 +443,16 @@ def write_to_csv(elosList):
     with open("Documents/Debate/SideBias/LDRankings.csv", "w") as fp:
         fp.write(add[:-1])
         
-
-add_tournament("Loyola", 4)
-add_tournament("SeasonOpener", 4)
-add_tournament("Grapevine", 4)
-add_tournament("Yale", 4)
-add_tournament("MidAmericaCup", 8)
-add_tournament("JackHowe", 2)
-add_tournament("Greenhill", 8)
+# third parameter is for time/weekend. just has to be sequential.
+add_tournament("Loyola", 4, 20240830)
+add_tournament("SeasonOpener", 4, 20240907)
+add_tournament("Grapevine", 4, 20240913)
+add_tournament("Yale", 4, 20240920)
+add_tournament("MidAmericaCup", 8, 20240927)
+add_tournament("JackHowe", 2, 20240928)
+add_tournament("Greenhill", 8, 20240919)
+add_tournament("NanoNagle", 4, 20241012) 
+add_tournament("DTA", 2, 20241012)
 
 negPercent = negWs/rounds
 negElimPercent = elimNegWs/elims
@@ -337,3 +461,7 @@ print("Elim Neg wins " +  str(negElimPercent) + ". Rounds: " + str(elimNegWs) + 
 
 elos = sorted(elos_dict.items(), key=lambda item: item[1], reverse=True)
 write_to_csv(elos)
+
+roundDictCSV()
+skeloElo()
+skeloGlicko()
